0ba572222f416040c3c6e8da4cc7efaca9221ebe
Mike <mick_mick_mick@icloud.com>
2023-09-05 09:01:57 +0400

    Initial commit

---
+++ src/test/java/com/mike/tagmessenger/TagMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.tagmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
